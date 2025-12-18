98d404bba6c0b1f76399ee7747701782c72d3033
Your Name <dan.weibren@gmail.com>
2025-12-18 09:52:51 +0200

    chore(widget): demo harness and app cleanup
    - Customer injection example and test harness
    - Update demo index.html
    - Remove Vue boilerplate components

---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    name: 'Home',
+}
+</script>
+
+<template>
+    <div class="main">
+        About
+        <section class="routes">
+            <RouterLink to="/about/team"><button>team</button></RouterLink>
+            <RouterLink to="/about/vis"><button>vis</button></RouterLink>
+        </section>
+        <RouterView />
+    </div>
+</template>
+
+<style scoped>
+    .routes{
+        display: grid;
+        grid-auto-flow: column;
+    }
+</style>
